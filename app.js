7cce07d1f3a0d79fc563411e08e447b12c614e98
Nicolas Lupien <niclupien@gmail.com>
2013-10-05 15:25:53 -0400

    views

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.configure( function() {
   app.use(express.bodyParser());
   app.use(express.logger());
   app.use(express.cookieParser());
+  app.set('view engine', 'jade');
+  app.set('views', __dirname + '/views');
   app.use(express.session({ secret: 'lf9023endkvj' }));
   app.use(express.static(__dirname + '/public'));
 });
@@ -19,6 +21,10 @@ app.get('/upload', function(req, res){
     console.log(file);
   });
 }); 
+
+app.get('/', function(req, res) {
+  res.render('home', { hello: 'world'}); 
+});
 
 app.listen(3000);
 console.log("Listening on port 3000")
