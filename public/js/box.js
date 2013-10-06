067a737adc38a321078e00f71345011d4318371f
Nicolas Lupien <niclupien@gmail.com>
2013-10-05 22:38:55 -0400

    ...

--- public/js/box.js
+++ public/js/box.js
@@ -1,13 +1,11 @@
 var password;
 
 function startCountdown(){
-  console.log("maman");
   var timeLimit = 60 * 1000; // in sec
   var expiration_time = new Date().getTime() + timeLimit;
   var hours, minutes, seconds;
   var countdown = document.getElementById('countdown');
   var timer = setInterval(function() {
-   console.log("patate");
     var current_time = new Date().getTime();
     var seconds_left = (expiration_time - current_time) / 1000
     hours = parseInt(seconds_left / 3600);
@@ -15,7 +13,7 @@ function startCountdown(){
     minutes = parseInt(seconds_left / 60);
     seconds = parseInt(seconds_left % 60);
     countdown.innerHtml = hours + "h, " + minutes + "m, " + seconds + "s";
-    console.log(hours + "h, " + minutes + "m, " + seconds + "s");
+    //console.log(hours + "h, " + minutes + "m, " + seconds + "s");
     if (seconds_left == 0)
       clearInterval(timer);
   }, 1000);
